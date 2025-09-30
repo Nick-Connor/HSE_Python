44156b51767b6e3038387dff69f3b8b87e91f58b
Nikita Gaidar <nikitagaydar@gmail.com>
2025-09-30 20:42:45 +0300

    Initial commit

---
+++ HW_2.py
@@ -0,0 +1,18 @@
+# Task 3
+word = str(input("Write a word\n"))
+
+print(word[(len(word) - 1) // 2 : (len(word) + 2) // 2])
+
+# Task 4
+boys = ["Peter", "Alex", "John", "Arthur", "Richard"]
+girls = ["Kate", "Liza", "Kira", "Emma", "Trisha"]
+
+if len(boys) != len(girls):
+    print("Внимание, кто-то может остаться без пары!")
+else:
+    boys_sorted = sorted(boys)
+    girls_sorted = sorted(girls)
+
+    print("Идеальные пары:")
+    for boy, girl in zip(boys_sorted, girls_sorted):
+        print(f"{boy} и {girl}")
